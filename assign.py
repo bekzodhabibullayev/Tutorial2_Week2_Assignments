30ce4d1a366413fc11ca024839578ef020826a9e
Bekzod Habibullayev <bekzodhabibullayev68@gmail.com>
2025-10-06 21:37:12 +0500

    XENO-BIOLOGY FIELD REPORT generator

---
+++ assign.py
@@ -0,0 +1,24 @@
+creature_name = input("Creature_name: ")
+discovered_planet = input("Discovered planet: ")
+creature_diet = input("Creature Diet: ")
+scientist_name = input("Professor: ")
+discovery_year = input("Year: ")
+print("================================= ")
+print("XENO-BIOLOGY FIELD REPORT: ")
+print("================================= ")
+print("Scientist:        Dr." +  scientist_name  )
+print("Discovery Year:   " +  discovery_year )
+print("                                   ")
+print("---------------------------------" )
+print("Creature Name:     " +creature_name)
+print("Home Planet:       " +discovered_planet )
+print("Primary Diet:      " +creature_diet  )
+print()
+print("Scientific Designation: "   + discovered_planet + "-" +  creature_name + "-" +  discovery_year )
+print("=================================")
+
+# creature_name = Terranova
+# discovered_planet = Pluton
+# creature_diet = Plov
+# Scientist_name = Bekzod Habibullayev
+# Discovery_year = 2045
